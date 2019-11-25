d4daafa5ec5f37ee6a39ca3a980eae3674781b61
fahturr <>
2019-11-25 16:34:23 +0700

    harini

--- src/java/Pertemuan11/OlahAddPegawai.java
+++ src/java/Pertemuan11/OlahAddPegawai.java
@@ -7,14 +7,13 @@ package Pertemuan11;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import pt_pcr.Pegawai;
 import pt_pcr.PegawaiHome;
-import Pertemuan11.OlahAddPegawai;
-import javax.servlet.RequestDispatcher;
 
 /**
  *
@@ -36,27 +35,24 @@ public class OlahAddPegawai extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
         RequestDispatcher dispatcher;
-
+        
         try {
-            String nip =request.getParameter("nip");
-            String nama =request.getParameter("nama");
-            String alamat =request.getParameter("alamat");
-            String jabatan =request.getParameter("jabatan");
-            
+            String nip = request.getParameter("nip");
+            String nama = request.getParameter("nama");
+            String alamat = request.getParameter("alamat");
+            String jabatan = request.getParameter("jabatan");
+        
             Pegawai pegawai = new Pegawai();
             pegawai.setNip(nip);
             pegawai.setNamaPegawai(nama);
             pegawai.setAlamat(alamat);
             pegawai.setJabatan(jabatan);
             
-            PegawaiHome home = new PegawaiHome();
+            PegawaiHome pegawaiBean = new PegawaiHome();
             request.setAttribute("dataPegawai", pegawai);
             
-            if (home.create(pegawai) == true) {
-                dispatcher = getServletContext().getRequestDispatcher("/sukses.jsp");
-                dispatcher.forward(request, response);
-            } else {
-                dispatcher = getServletContext().getRequestDispatcher("/gagal.jsp");
+            if (pegawaiBean.create(pegawai) == true) {
+                dispatcher = request.getRequestDispatcher("PT_PCR/suskses.jsp");
                 dispatcher.forward(request, response);
             }
         } finally {
